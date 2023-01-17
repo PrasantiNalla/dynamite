c73267800c4f719e242a082ccffd7a0af93139c5
Joe Fry <103355865+OKPersonalComputer@users.noreply.github.com>
2023-01-17 14:56:15 +0000

    Sort formatting

--- myBot.js
+++ myBot.js
@@ -7,21 +7,21 @@ class Bot {
 
         const p2DCount = gamestate.rounds.reduce((a, b) => b.p2 === 'D' ? a + 1 : a, 0);
 
-        if (( p1DCount < 100) && lastRound && (lastRound.p1 === lastRound.p2)) {
+        if ((p1DCount < 100) && lastRound && (lastRound.p1 === lastRound.p2)) {
             return 'D';
         }
 
         else if ((p1DCount < 100) && (gamestate.rounds.length > 1000)) {
-            if ((p2DCount < 100)&& p2DCount>5) {
+            if ((p2DCount < 100) && p2DCount > 5) {
                 let randomIndex = Math.floor(5 * Math.random());
                 return ['R', 'P', 'S', 'W', 'D'][randomIndex];
             } else {
                 let randomIndex = Math.floor(4 * Math.random());
                 return ['R', 'P', 'S', 'D'][randomIndex];
             }
-        } 
+        }
 
-        else if (p2DCount < 100 && p2DCount>5) {
+        else if (p2DCount < 100 && p2DCount > 5) {
             let randomIndex = Math.floor(4 * Math.random());
             return ['R', 'P', 'S', 'W'][randomIndex];
         }
--- randomBot1.js
+++ randomBot1.js
@@ -1,13 +1,11 @@
 class Bot {
 
-    
+
     makeMove(gamestate) {
         let randomIndex = Math.floor(3 * Math.random())
         return ['R', 'P', 'S'][randomIndex];
-         console.log(gamestate.round);
+        console.log(gamestate.round);
     }
-  
-    
 }
 
 module.exports = new Bot();
